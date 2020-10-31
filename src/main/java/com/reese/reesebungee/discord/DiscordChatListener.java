6708b735ed7fe89623db6448b8833bf3763689c9
Reese Norris <reese.norris@gmail.com>
2020-10-30 17:41:27 -0700

    add online players command

--- src/main/java/com/reese/reesebungee/discord/DiscordChatListener.java
+++ src/main/java/com/reese/reesebungee/discord/DiscordChatListener.java
@@ -1,9 +1,15 @@
 package com.reese.reesebungee.discord;
 
 import com.reese.reesebungee.ReeseBungee;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
+
+import java.awt.*;
+import java.util.Collection;
 
 public class DiscordChatListener extends ListenerAdapter {
 
@@ -15,7 +21,31 @@ public class DiscordChatListener extends ListenerAdapter {
 
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
-        if (event.getChannel().getIdLong() == this.reeseBungee.discord.mainChannelID && !event.getAuthor().equals(this.reeseBungee.discord.jda.getSelfUser())) {
+
+        if (event.getChannel().getIdLong() != this.reeseBungee.discord.mainChannelID || event.getAuthor().equals(this.reeseBungee.discord.jda.getSelfUser())) {
+            return;
+        }
+
+        if (event.getMessage().getContentStripped().equals("ca!online")) {
+            int currentPlayerCount = this.reeseBungee.getProxy().getOnlineCount();
+            Collection<ProxiedPlayer> players = this.reeseBungee.getProxy().getPlayers();
+            EmbedBuilder embed = new EmbedBuilder();
+            embed.setTitle(currentPlayerCount + " players connected");
+            embed.setColor(Color.RED);
+
+            if (currentPlayerCount > 0) {
+                embed.setColor(Color.GREEN);
+                StringBuilder playerList = new StringBuilder();
+                for (ProxiedPlayer player : players) {
+                    playerList.append(player.getDisplayName() + "\n");
+                }
+                MessageEmbed.Field field = new MessageEmbed.Field("List", playerList.toString(), false);
+                embed.addField(field);
+            }
+            event.getTextChannel().sendMessage(embed.build()).queue();
+        }
+        else {
+            // not a command -> forward to minecraft
             StringBuilder str = new StringBuilder(String.valueOf(ChatColor.DARK_PURPLE));
             str.append("[Discord] ");
             str.append(ChatColor.RESET);
